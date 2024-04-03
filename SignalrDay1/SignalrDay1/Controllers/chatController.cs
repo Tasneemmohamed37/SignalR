736a2a8cb9cad0f5fa84644cc963d703fe55d2fa
tasnemmuhammad37 <tasnemmuhammad37@gmail.com>
2024-04-03 05:55:00 +0200

    SignalR session 01

---
+++ SignalrDay1/SignalrDay1/Controllers/chatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalrDay1.Controllers
+{
+    public class chatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalrDay1/SignalrDay1/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace SignalrDay1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void SendMsg(string name, string msg)
+        {
+            // save msg in db
+
+            // send new msg to all online clients
+            Clients.All.SendAsync("newmessage", name, msg);
+        }
+
+    }
+}
